d09ee978adb4a528ec36786c9d78c9740b83bfad
zandersantos1 <zainzedd@gmail.com>
2025-03-15 13:35:09 -0500

    Added associations and validations to the Ability Model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,2 +1,6 @@
 class Ability < ApplicationRecord
+  has_many :monster_ability
+
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true, uniqueness: true
 end
