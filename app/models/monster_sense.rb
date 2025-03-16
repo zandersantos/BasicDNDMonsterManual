b346dc650caecc612994479c1f0181bbf078aa11
zandersantos1 <zainzedd@gmail.com>
2025-03-16 14:26:36 -0500

    Created a Monster_Sense table and removed the previous migration for the old Monster_Sense

---
+++ app/models/monster_sense.rb
@@ -0,0 +1,4 @@
+class MonsterSense < ApplicationRecord
+  belongs_to :monster
+  belongs_to :sense
+end
--- db/migrate/20250316192604_create_monster_senses.rb
+++ db/migrate/20250316192604_create_monster_senses.rb
@@ -1,9 +1,9 @@
 class CreateMonsterSenses < ActiveRecord::Migration[8.0]
   def change
     create_table :monster_senses do |t|
-      t.string :sense_type
       t.string :sense_range
       t.references :monster, null: false, foreign_key: true
+      t.references :sense, null: false, foreign_key: true
 
       t.timestamps
     end
