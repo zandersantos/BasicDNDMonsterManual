27e3415249064a6dee60f437f542d89628615808
zandersantos1 <zainzedd@gmail.com>
2025-03-17 01:47:56 -0500

    Updated the seeds.rb file to comment out previous queries

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,81 +1,74 @@
 require "csv"
 
-#Monster Create
+#MONSTER CREATION
+
+# MonsterSense.delete_all
+# Monster.delete_all
+# Sense.delete_all
+
+# filenamemonster = Rails.root.join "db/monstersdatafile.csv"
+# puts "Reading in the file from here #{filenamemonster}"
+
+# csv_data = File.read(filenamemonster)
+# monsters = CSV.parse(csv_data, headers: true, encoding: 'utf-8')
+
+# monsters.each do |monster|
+
+#   # Create monster record with armor_class value
+#   monster_record = Monster.create(name: monster["name"], armour_class: monster["armor_class"], hitpoints: monster["hit_points"], hit_dice: monster["hit_dice"], img_url: monster["image_url"])
+
+#   if monster_record.valid?
+#     puts "#{monster["name"]} Created"
+#   else
+#     puts "Unable to create the monster name #{monster["name"]}"
+#     puts "Errors: #{monster_record.errors.full_messages.join(", ")}"
+#   end
+# end
+
+# puts "Created #{Monster.count} Names"
 
 
-MonsterSense.delete_all
-Monster.delete_all
-Sense.delete_all
+#SENSES CREATION
+# filenamesense= Rails.root.join "db/sensedata.csv"
+# puts "Reading in the file from here #{filenamesense}"
+
+# csv_data = File.read(filenamesense)
+# senses = CSV.parse(csv_data, headers: true, encoding: 'utf-8')
+
+# senses.each do |sense|
+
+#   # Create monster record with armor_class value
+#   sense_record = Sense.create(sense_type: sense["sense_type"])
+
+#   if sense_record.valid?
+#     puts "#{sense["sense_type"]} Created"
+#   else
+#     puts "Unable to create the sense type #{sense["sense_type"]}"
+#     puts "Errors: #{sense_record.errors.full_messages.join(", ")}"
+#   end
+# end
+
+# puts "Created #{Sense.count} Senses"
 
 
-filenamemonster = Rails.root.join "db/monstersdatafile.csv"
-puts "Reading in the file from here #{filenamemonster}"
+#MONSTER SENSE CREATION
+# filenamemonstersense= Rails.root.join "db/monstersensedatafile.csv"
+# puts "Reading in the file from here #{filenamemonstersense}"
 
-csv_data = File.read(filenamemonster)
-monsters = CSV.parse(csv_data, headers: true, encoding: 'utf-8')
+# csv_data = File.read(filenamemonstersense)
+# monstersenses = CSV.parse(csv_data, headers: true, encoding: 'utf-8')
 
-monsters.each do |monster|
+# monstersenses.each do |monstersense|
 
+#    # Create monster record with armor_class value
+#   monstersense_record = MonsterSense.create(monster_id: monstersense["monster_id"], sense_id: monstersense["sense_type"], sense_range: monstersense["sense_range"])
 
-    # Create monster record with armor_class value
-  monster_record = Monster.create(name: monster["name"], armour_class: monster["armor_class"], hitpoints: monster["hit_points"], hit_dice: monster["hit_dice"], img_url: monster["image_url"])
+#   if monstersense_record.valid?
+#     puts "#{monstersense["sense_range"]} Created"
+#   else
+#     puts "Unable to create the sense type #{monstersense["sense_range"]}"
+#     puts "Errors: #{monstersense_record.errors.full_messages.join(", ")}"
+#   end
+# end
 
-
-  if monster_record.valid?
-    puts "#{monster["name"]} Created"
-  else
-    puts "Unable to create the monster name #{monster["name"]}"
-    puts "Errors: #{monster_record.errors.full_messages.join(", ")}"
-  end
-end
-
-puts "Created #{Monster.count} Names"
-
-
-filenamesense= Rails.root.join "db/sensedata.csv"
-puts "Reading in the file from here #{filenamesense}"
-
-csv_data = File.read(filenamesense)
-senses = CSV.parse(csv_data, headers: true, encoding: 'utf-8')
-
-senses.each do |sense|
-
-
-    # Create monster record with armor_class value
-  sense_record = Sense.create(sense_type: sense["sense_type"])
-
-
-  if sense_record.valid?
-    puts "#{sense["sense_type"]} Created"
-  else
-    puts "Unable to create the sense type #{sense["sense_type"]}"
-    puts "Errors: #{sense_record.errors.full_messages.join(", ")}"
-  end
-end
-
-puts "Created #{Sense.count} Senses"
-
-
-
-filenamemonstersense= Rails.root.join "db/monstersensedatafile.csv"
-puts "Reading in the file from here #{filenamemonstersense}"
-
-csv_data = File.read(filenamemonstersense)
-monstersenses = CSV.parse(csv_data, headers: true, encoding: 'utf-8')
-
-
-monstersenses.each do |monstersense|
-
-    # Create monster record with armor_class value
-  monstersense_record = MonsterSense.create(monster_id: monstersense["monster_id"], sense_id: monstersense["sense_type"], sense_range: monstersense["sense_range"])
-
-
-  if monstersense_record.valid?
-    puts "#{monstersense["sense_range"]} Created"
-  else
-    puts "Unable to create the sense type #{monstersense["sense_range"]}"
-    puts "Errors: #{monstersense_record.errors.full_messages.join(", ")}"
-  end
-end
-
-puts "Created #{MonsterSense.count} MonsterSenses"
+# puts "Created #{MonsterSense.count} MonsterSenses"
