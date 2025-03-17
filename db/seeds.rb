d9fa96c52704d964aea7611a47d1a312258d34ca
zandersantos1 <zainzedd@gmail.com>
2025-03-17 00:16:25 -0500

    Populated the Sense table, and added a monster_sense data file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,17 @@
 require "csv"
 
+#Monster Create
+
+
+
 Monster.delete_all
+Sense.delete_all
+Monster_Sense.delete_all
 
-filename = Rails.root.join "db/monstersdatafile.csv"
-puts "Reading in the file from here #{filename}"
+filenamemonster = Rails.root.join "db/monstersdatafile.csv"
+puts "Reading in the file from here #{filenamemonster}"
 
-csv_data = File.read(filename)
+csv_data = File.read(filenamemonster)
 monsters = CSV.parse(csv_data, headers: true, encoding: 'utf-8')
 
 monsters.each do |monster|
@@ -24,3 +30,27 @@ monsters.each do |monster|
 end
 
 puts "Created #{Monster.count} Names"
+
+
+filenamesense= Rails.root.join "db/sensedata.csv"
+puts "Reading in the file from here #{filenamesense}"
+
+csv_data = File.read(filenamesense)
+senses = CSV.parse(csv_data, headers: true, encoding: 'utf-8')
+
+senses.each do |sense|
+
+
+    # Create monster record with armor_class value
+  sense_record = Sense.create(sense_type: sense["sense_type"])
+
+
+  if sense_record.valid?
+    puts "#{sense["sense_type"]} Created"
+  else
+    puts "Unable to create the sense type #{sense["sense_type"]}"
+    puts "Errors: #{sense_record.errors.full_messages.join(", ")}"
+  end
+end
+
+puts "Created #{Sense.count} Names"
