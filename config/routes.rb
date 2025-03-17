e1678681367cf4a9ef534d5cd9d8265d8f809f3c
zandersantos1 <zainzedd@gmail.com>
2025-03-17 17:11:53 -0500

    Updated the routes to be more restful for monsters

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get "monsters/index"
-  get "monsters/show"
+  resources :monsters, only: [ :index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
