0ca9ab5eec90c9f25ec376b7c8153dd6d811e654
zandersantos1 <zainzedd@gmail.com>
2025-03-17 17:32:15 -0500

    Updated the Actions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get "actions/index"
-  get "actions/show"
-  resources :monsters, only: [ :index, :show]
+  resources :monsters, only: [ :index, :show ]
+  resources :actions, only: [ :index, :show ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
