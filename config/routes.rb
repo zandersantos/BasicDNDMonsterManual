3ed3734370b3be3ed38bb3f791ee83ee8bfd82d1
zandersantos1 <zainzedd@gmail.com>
2025-03-17 18:29:53 -0500

    Added a home controller with an index action and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "home/index"
   resources :monsters, only: [ :index, :show ]
   resources :actions, only: [ :index, :show ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
