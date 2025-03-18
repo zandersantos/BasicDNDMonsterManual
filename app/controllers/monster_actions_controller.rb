5c91a3ef2cd93ba80dfdbdc473b2facf2d091ed1
zandersantos1 <zainzedd@gmail.com>
2025-03-18 10:14:37 -0500

    Updated the monsters controller and show to include the monster_actions and monster_actions description

--- app/controllers/monster_actions_controller.rb
+++ app/controllers/monster_actions_controller.rb
@@ -3,5 +3,6 @@ class MonsterActionsController < ApplicationController
   end
 
   def show
+    @monsteractions = MonsterAction.joins(:action).where(monster_id: params[:id]).select('monster_actions.description, actions.name AS action_name').order('actions.name ASC')
   end
 end
--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -5,5 +5,12 @@ class MonstersController < ApplicationController
 
   def show
     @monsters = Monster.find(params[:id])
+
+    @monsteractions = MonsterAction
+    .joins(:action)
+    .where(monster_id: params[:id])
+    .select('monster_actions.id, monster_actions.description, monster_actions.action_id, actions.name AS action_name')
+    .order('actions.name ASC')
+
   end
 end
