842025ee9d5ca82aa413f3cffb48e4297df394a3
zandersantos1 <zainzedd@gmail.com>
2025-03-19 00:03:54 -0500

    Changed the Monster views to show the associated image

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   root to: "home#index"
   resources :monsters, only: [ :index, :show ]
   resources :actions, only: [ :index, :show ]
+  resources :senses, only: [ :index, :show ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
