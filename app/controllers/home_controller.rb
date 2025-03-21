976c5ce72e2f9a5fd75493d334ce075d189c8b8f
zandersantos1 <zainzedd@gmail.com>
2025-03-20 19:19:16 -0500

    Updated Senses Model, Home Controller, and Home View to put top 3 senses

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @monsters = Monster.order("name ASC").limit(10)
     @actions = Action.order_by_monster_count.includes(:monsters).limit(10)
+    @senses = Sense.order_by_monster_count.includes(:monsters).limit(3)
   end
 end
--- app/models/sense.rb
+++ app/models/sense.rb
@@ -3,4 +3,20 @@ class Sense < ApplicationRecord
   has_many :monsters, through: :monster_senses
 
   validates :sense_type, presence: true, uniqueness: true
+  def self.order_by_monster_count
+    #SELECT actions.name, COUNT(monster_actions.monster_id) AS monster_count
+      #FROM monster_actions
+       # LEFT OUTER JOIN actions ON monster_actions.action_id = actions.id
+      #GROUP BY monster_actions.action_id
+      #ORDER BY monster_count DESC;
+
+      self.select("senses.id")
+          .select("senses.sense_type")
+          .select("COUNT(monster_senses.monster_id) AS monster_count")
+          .left_joins(:monster_senses)
+          .group("senses.id")
+          .order("monster_count DESC")
+
+  end
+
 end
